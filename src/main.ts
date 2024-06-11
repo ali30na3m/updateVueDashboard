9fbf1e5ae3d78d70d5437130790a05fd8e287755
ali30na3m <30naali1382@gmail.com>
2024-06-11 16:35:03 +0330

    router isCompleted

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import router from './router'
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(router)
 app.mount('#app')
